841a7b25cfdf3c5029d1b18b1f5f231f13842bf7
digad1 <supers2002bog@gmail.com>
2023-05-22 21:24:09 +0300

    Frontend commit

--- backend/passhash.py
+++
@@ -1,5 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-raw_password = "Passw0rd"  # replace with the actual password
-hashed_password = generate_password_hash(raw_password)
-print(hashed_password)
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+const app = express();
+app.use(serveStatic(path.join(__dirname, 'dist'))); // Serve static files from 'dist' directory
+
+const port = process.env.PORT || 5000; // Listen on the appropriate port
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
